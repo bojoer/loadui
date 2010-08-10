ce2e0821ba0ca9c2c1285fdeeaa5b9a02aa14cd9
Predrag Vucetic <pregrad.vucetic@smartbear.com>
2010-08-10 15:23:57 +0000

    777

---
+++ loadui-project/loadui-groovy-components/src/main/groovy/Scheduler.groovy
@@ -0,0 +1,229 @@
+// Copyright 2010 eviware software ab
+// 
+// Licensed under the EUPL, Version 1.1 or - as soon they will be approved by the European Commission - subsequent
+// versions of the EUPL (the "Licence");
+// You may not use this work except in compliance with the Licence.
+// You may obtain a copy of the Licence at:
+// 
+// http://ec.europa.eu/idabc/eupl5
+// 
+// Unless required by applicable law or agreed to in writing, software distributed under the Licence is
+// distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the Licence for the specific language governing permissions and limitations
+// under the Licence.
+// 
+
+/**
+ * Schedule the start and stop of a trigger component.
+ * On Start starts timer and when StartAt reached send one START message to 
+ * attached component. When Duration expires one STOP message is send to each 
+ * attached component.
+ * Repeat option repeats whole process if counter limit not set.
+ * 
+ * On Stop it sends STOP message to attached components and stops timer.
+ *
+ * On Reset it just reset timer and stops it.
+ *
+ * @help http://www.loadui.org/Schedulers/interval.html
+ * @category scheduler
+ * @nonBlocking true
+ * @dependency org.quartz-scheduler:quartz:1.8.3
+ * 
+ */
+
+import com.eviware.loadui.api.events.ActionEvent
+import com.eviware.loadui.api.model.CanvasItem
+import com.eviware.loadui.api.counter.CounterHolder
+
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
+import com.eviware.loadui.api.component.categories.TriggerCategory
+import com.eviware.loadui.api.events.PropertyEvent
+import com.eviware.loadui.util.layout.IntervalModel
+import com.eviware.loadui.util.ScheduledExecutor
+import org.quartz.Scheduler
+import org.quartz.CronTrigger
+import org.quartz.impl.StdSchedulerFactory
+import org.quartz.JobExecutionContext
+import org.quartz.JobExecutionException
+import org.quartz.Job
+import org.quartz.JobDetail
+import java.util.Calendar
+import org.quartz.listeners.JobListenerSupport
+
+createProperty( 'day', String, "Every day" )
+createProperty( 'hour', Long, -1 )
+createProperty( 'minute', Long, -1 )
+createProperty( 'second', Long, 0 )
+def duration = createProperty( 'duration', Long, -1 )
+def unit = createProperty( 'unit', String, 'Sec' )
+def repeatCount = createProperty( 'repeatCount', Long, -1 )
+
+def canvas = getCanvas()
+
+def startMessage = newMessage()
+startMessage[TriggerCategory.ENABLED_MESSAGE_PARAM] = true
+sendStart = { send( outputTerminal, startMessage ) }
+
+def stopMessage = newMessage()
+stopMessage[TriggerCategory.ENABLED_MESSAGE_PARAM] = false
+sendStop = { send( outputTerminal, stopMessage ) }
+
+def endFuture = null
+def executor = ScheduledExecutor.instance
+
+def counter = 0
+def durationHolder = 0
+def repeatHolder = 0
+
+def scheduler = new StdSchedulerFactory().getScheduler()
+scheduler.addJobListener(new JobListenerSupport()
+{
+	String getName(){
+		"jobListener"
+	}
+	void jobWasExecuted(JobExecutionContext context, JobExecutionException jobException) {
+		sendStart()
+		setDuration()
+		counter++
+		println("------ counter ${counter}")
+		if(repeatHolder > -1 && counter >= repeatHolder){
+			unschedule()
+		}
+	}
+})
+
+setDuration = {
+	endFuture?.cancel( true )
+	if(durationHolder > -1){
+		endFuture = executor.schedule( sendStop, durationHolder, TimeUnit.MILLISECONDS )
+		println("-----duration (seconds) ${durationHolder/1000}")
+	}
+	else{
+		println("-----duration INFINITE")
+	}
+}
+
+def paused = false
+
+def trigger = null
+class SchedulerJob implements Job {
+	void execute(JobExecutionContext context) throws JobExecutionException {}
+}
+def job = new JobDetail("job1", "group1", SchedulerJob.class)
+job.addJobListener("jobListener")
+
+addEventListener( ActionEvent ) { event ->
+	if( event.key == CanvasItem.START_ACTION) {
+		if(paused) 
+			scheduler?.resumeJob("job1", "group1")
+		else{
+			setScheduler()
+			scheduler?.start()
+		}
+		paused = false
+		println("-----START")
+	}
+	else if( event.key == CanvasItem.STOP_ACTION) {
+		scheduler?.pauseJob("job1", "group1")
+		paused = true
+		println("-----STOP")
+	}
+	else if( event.key == CanvasItem.COMPLETE_ACTION) {
+		counter = 0
+		paused = false
+		unschedule()
+		println("-----COMPLETE")
+	}
+	else if(event.key == CounterHolder.COUNTER_RESET_ACTION){
+		println("-----RESET")
+		paused = false
+		unschedule()
+		counter = 0
+	}
+}
+
+addEventListener( PropertyEvent ) { event ->
+	if( event.property in [ day, hour, minute, second, duration, unit, repeatCount ] ) {
+		//if( !canvas.running ) setScheduler()
+	}
+}
+
+setScheduler = {
+	def triggerPattern = ""
+	if(second.value == -1){
+		triggerPattern += "* "
+	}
+	else{
+		triggerPattern += "${second.value} "
+	}
+	if(minute.value == -1){
+		triggerPattern += "* "
+	}
+	else{
+		triggerPattern += "${minute.value} "
+	}
+	if(hour.value == -1){
+		triggerPattern += "* "
+	}
+	else{
+		triggerPattern += "${hour.value} "
+	}
+	triggerPattern += "? * "
+	if(day.value.equals("Every day")){
+		triggerPattern += "* "
+	}
+	else{
+		triggerPattern += "${day.value.substring(0,3).toUpperCase()} "
+	}
+	
+	println("------ quartz pattern ${triggerPattern}")
+	
+	unschedule()
+	scheduler.addJob(job, true)
+	trigger = new CronTrigger("trigger1", "group1", "job1", "group1", triggerPattern)
+	scheduler.scheduleJob(trigger)
+	
+	repeatHolder = repeatCount.value
+	def mul = 1000
+	if(unit.value.equals("Min")){
+		mul *= 60
+	}
+	else if(unit.value.equals("Hour")){
+		mul *= 3600
+	}
+	durationHolder = duration.value * mul
+}
+
+unschedule = {
+	try{
+		scheduler.unscheduleJob("trigger1", "group1")
+	}
+	catch(Exception e){}
+}
+
+onRelease = {
+	cancelAll()
+}
+
+cancelAll = {
+	scheduler.shutdown()
+	endFuture?.cancel( true )
+	endFuture = null
+}
+
+layout() {
+    property( property: day, widget: 'comboBox', label: 'Day', options: ['Every day', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'], constraints: 'w 100!' )
+    separator( vertical: true )
+    property( property: hour, label: 'Hour', min: -1, max: 23)
+    property( property: minute, label: 'Min', min: -1, max: 59 )
+	property( property: second, label: 'Sec', min: -1, max: 59 )
+    separator( vertical: true )
+    property( property: duration, label: 'Duration', min: -1 )
+	property( property:unit, label: 'Unit', options: ['Sec','Min','Hour'] )
+	separator( vertical: true )
+    property( property: repeatCount, label: 'Repeat', min: -1)
+}
+
+//setScheduler()
