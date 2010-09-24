1a4546043dc27175641f7f31bc7fb4426ac14773
Dain Nilsson <dain.nilsson@smartbear.com>
2010-09-24 09:08:09 +0000

    Fixed Analysis and Output Component LED blinking.

--- loadui-project/loadui-component/src/main/java/com/eviware/loadui/impl/component/categories/AnalysisBase.java
+++ loadui-project/loadui-component/src/main/java/com/eviware/loadui/impl/component/categories/AnalysisBase.java
@@ -17,15 +17,16 @@ package com.eviware.loadui.impl.component.categories;
 
 import com.eviware.loadui.api.component.ComponentContext;
 import com.eviware.loadui.api.component.categories.AnalysisCategory;
-import com.eviware.loadui.api.events.ActionEvent;
-import com.eviware.loadui.api.events.BaseEvent;
-import com.eviware.loadui.api.events.EventHandler;
-import com.eviware.loadui.api.events.PropertyEvent;
-import com.eviware.loadui.api.model.CanvasItem;
 import com.eviware.loadui.api.terminal.InputTerminal;
 import com.eviware.loadui.api.terminal.OutputTerminal;
 import com.eviware.loadui.api.terminal.TerminalMessage;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
 import com.eviware.loadui.impl.component.ActivityStrategies;
+import com.eviware.loadui.util.BeanInjector;
 
 /**
  * Base class for analysis components which defines base behavior which can be
@@ -35,24 +36,48 @@ import com.eviware.loadui.impl.component.ActivityStrategies;
  */
 public abstract class AnalysisBase extends BaseCategory implements AnalysisCategory
 {
+	private static final int BLINK_TIME = 1000;
+
 	private final InputTerminal inputTerminal;
 
-	private final ActivityListener listener = new ActivityListener();
+	private final ScheduledExecutorService executor;
+	private final Runnable activityRunnable;
+	private long lastMsg;
+	private ScheduledFuture<?> activityFuture;
+
 	/**
 	 * Constructs an AnalysisBase.
 	 * 
 	 * @param context
 	 *           A ComponentContext to bind the AnalysisBase to.
 	 */
-	public AnalysisBase(ComponentContext context)
+	public AnalysisBase( ComponentContext context )
 	{
-		super(context);
+		super( context );
+		executor = BeanInjector.getBean( ScheduledExecutorService.class );
 
-		inputTerminal = context.createInput(INPUT_TERMINAL, "Input Data to be Analysed");
+		inputTerminal = context.createInput( INPUT_TERMINAL, "Input Data to be Analysed" );
 
-		context.addEventListener( BaseEvent.class, listener );
-		fixActivityStrategy();
-		
+		getContext().setActivityStrategy( ActivityStrategies.ON );
+		activityRunnable = new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				long now = System.currentTimeMillis();
+				synchronized( activityRunnable )
+				{
+					if( lastMsg + BLINK_TIME <= now )
+					{
+						getContext().setActivityStrategy( ActivityStrategies.ON );
+						activityFuture = null;
+					}
+					else
+						activityFuture = executor.schedule( activityRunnable, BLINK_TIME - ( now - lastMsg ),
+								TimeUnit.MILLISECONDS );
+				}
+			}
+		};
 	}
 
 	/**
@@ -60,7 +85,7 @@ public abstract class AnalysisBase extends BaseCategory implements AnalysisCateg
 	 * 
 	 * @param message
 	 */
-	public abstract void analyze(TerminalMessage message);
+	public abstract void analyze( TerminalMessage message );
 
 	@Override
 	final public InputTerminal getInputTerminal()
@@ -69,19 +94,24 @@ public abstract class AnalysisBase extends BaseCategory implements AnalysisCateg
 	}
 
 	@Override
-	public void onRelease()
+	public void onTerminalMessage( OutputTerminal output, InputTerminal input, TerminalMessage message )
 	{
-		// TODO Auto-generated method stub
-		super.onRelease();
-		getContext().removeEventListener(BaseEvent.class, listener);
-	}
-
-	@Override
-	public void onTerminalMessage(OutputTerminal output, InputTerminal input, TerminalMessage message)
-	{
-		if (input == inputTerminal)
+		if( input == inputTerminal )
 		{
-			analyze(message);
+			lastMsg = System.currentTimeMillis();
+			if( activityFuture == null )
+			{
+				synchronized( activityRunnable )
+				{
+					if( activityFuture == null )
+					{
+						getContext().setActivityStrategy( ActivityStrategies.BLINKING );
+						activityFuture = executor.schedule( activityRunnable, BLINK_TIME, TimeUnit.MILLISECONDS );
+					}
+				}
+			}
+
+			analyze( message );
 		}
 	}
 
@@ -95,22 +125,5 @@ public abstract class AnalysisBase extends BaseCategory implements AnalysisCateg
 	final public String getColor()
 	{
 		return COLOR;
-	}
-
-	private void fixActivityStrategy()
-	{
-		getContext().setActivityStrategy(getContext().isRunning() ? ActivityStrategies.BLINKING : ActivityStrategies.ON);
-	}
-
-	private class ActivityListener implements EventHandler<BaseEvent>
-	{
-		@Override
-		public void handleEvent(BaseEvent event)
-		{
-			if (event instanceof ActionEvent
-					&& (event.getKey() == CanvasItem.START_ACTION || event.getKey() == CanvasItem.STOP_ACTION)
-					|| event instanceof PropertyEvent)
-				fixActivityStrategy();
-		}
 	}
 }
