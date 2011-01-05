de322c3ba7a78be6e474e6738c069299ddee1670
Dain Nilsson <dain.nilsson@smartbear.com>
2011-01-05 13:24:31 +0000

    Updates to Chart nodes. Moved style.css to controller only, since it is not needed for agents. Added some missing license headers.

--- loadui-http-client/src/main/java/com/eviware/loadui/impl/http/HttpClientProviderImpl.java
+++ loadui-http-client/src/main/java/com/eviware/loadui/impl/http/HttpClientProviderImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 eviware software ab
+ * 
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved by the European Commission - subsequent
+ * versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ * 
+ * http://ec.europa.eu/idabc/eupl5
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the Licence for the specific language governing permissions and limitations
+ * under the Licence.
+ */
 package com.eviware.loadui.impl.http;
 
 import org.eclipse.jetty.client.HttpClient;
@@ -6,7 +21,6 @@ import com.eviware.loadui.api.http.HttpClientProvider;
 
 public class HttpClientProviderImpl implements HttpClientProvider
 {
-
 	private HttpClient httpsClient;
 	private HttpClient httpClient;
 
@@ -27,5 +41,4 @@ public class HttpClientProviderImpl implements HttpClientProvider
 	{
 		return httpClient;
 	}
-
 }
