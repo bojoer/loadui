bab8984a7959e497012c4db1bfc6e1d67f95072b
Dain Nilsson <dain.nilsson@smartbear.com>
2011-01-07 14:56:00 +0000

    Added ReleasableUtils. Refactored some classes to use it. Added JavaDoc comments to StringUtils.

--- loadui-project/loadui-util/src/main/java/com/eviware/loadui/util/StringUtils.java
+++ loadui-project/loadui-util/src/main/java/com/eviware/loadui/util/StringUtils.java
@@ -19,25 +19,57 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * Utility class for Strings.
+ * 
+ * @author dain.nilsson
+ */
 public class StringUtils
 {
 	public static final String LINE_SEPARATOR = System.getProperty( "line.separator" );
 
+	/**
+	 * Checks of the given String is null or empty.
+	 * 
+	 * @param string
+	 * @return
+	 */
 	public static boolean isNullOrEmpty( String string )
 	{
 		return string == null || string.equals( "" );
 	}
 
+	/**
+	 * Capitalizes the first character of a String. If the String is null, null
+	 * is returned.
+	 * 
+	 * @param string
+	 * @return
+	 */
 	public static String capitalize( String string )
 	{
 		return isNullOrEmpty( string ) ? string : string.substring( 0, 1 ).toUpperCase() + string.substring( 1 );
 	}
 
+	/**
+	 * Converts line separators in a String to the system default (defined in the
+	 * System Property "line.separator").
+	 * 
+	 * @param string
+	 * @return
+	 */
 	public static String fixLineSeparators( String string )
 	{
 		return string == null ? null : string.replaceAll( "\\r\\n|\\r|\\n", LINE_SEPARATOR );
 	}
 
+	/**
+	 * Converts a Collection (or array, or varargs) of Strings into a single
+	 * String, joined by the system default line separator characters.
+	 * 
+	 * @param lines
+	 * @return
+	 */
 	public static String multiline( String... lines )
 	{
 		if( lines.length == 0 )
@@ -52,6 +84,13 @@ public class StringUtils
 		return sb.toString();
 	}
 
+	/**
+	 * Serializes a collection of Strings into a single string, which can later
+	 * be deserialized to give a List of the original values. Order is preserved.
+	 * 
+	 * @param strings
+	 * @return
+	 */
 	public static String serialize( Collection<String> strings )
 	{
 		StringBuilder sb = new StringBuilder();
@@ -61,6 +100,12 @@ public class StringUtils
 		return sb.toString();
 	}
 
+	/**
+	 * Deserializes a serialized String back into a List of the original values.
+	 * 
+	 * @param serialized
+	 * @return
+	 */
 	public static List<String> deserialize( String serialized )
 	{
 		List<String> strings = new ArrayList<String>();
