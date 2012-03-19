90893e820b9674559087ccaed8f3ebdbb2f2dd52
Henrik Olsson <henrik.olsson@smartbear.com>
2012-03-19 13:43:03 +0000

    LUCO-2972

--- loadui-project/loadui-groovy-component-pack/src/main/groovy/RampSequence.groovy
+++ loadui-project/loadui-groovy-component-pack/src/main/groovy/RampSequence.groovy
@@ -13,28 +13,46 @@ import java.util.concurrent.TimeUnit
 createProperty( 'rampLength', Long, 10 ) { calculateAcceleration() }
 createProperty( 'peakRate', Long, 10 ) { calculateAcceleration() }
 createProperty( 'peakLength', Long, 10 )
+createProperty( 'peakRateUnit', String, 'sec' ) { calculateAcceleration(); redraw() }
 
+future = null
 startTime = 0
 calculateAcceleration()
 
 onAction( 'START' ) {
+	calculateAcceleration()
 	startTime = currentTime()
+	hasPeaked = false
 	scheduleNext()
 }
 
-
 onAction( 'STOP' ) {
 	future?.cancel( true )
+	cancellingFuture?.cancel( true )
+	startTime = null
 }
 
 scheduleNext = {
-	t0 = getT0()
-	if( t0 > 0 ) {
+	def t0 = getT0()
+	
+	if( t0 >= rampLength.value && !hasPeaked ) {
+		hasPeaked = true
+		def delay = 1000000/peakRate.value
+		future = scheduleAtFixedRate( { trigger() }, delay, delay, TimeUnit.MICROSECONDS )
+		cancellingFuture = schedule( {
+			future?.cancel( true )
+			a = a*-1
+			scheduleNext( rampLength.value )
+		}, peakLength.value, TimeUnit.SECONDS )
+	} else if( t0 >= 0 ) {
 		t1 = Math.sqrt( 2/a + t0**2 )
+		
+		future?.cancel( true )
+		def diff = Math.abs( t1 - getT0() )
 		future = schedule( {
 				trigger()
-				scheduleNext()
-			}, ( (t1-t0)*1000 ).intValue(), TimeUnit.MILLISECONDS )
+				scheduleNext( t1 )
+			}, ( diff*1000000) as long, TimeUnit.MICROSECONDS )
 	}
 }
 
@@ -45,23 +63,32 @@ def getT0() {
 		return startTime + rampLength.value*2 + peakLength.value - currentTime()
 	if( relativeTime >= rampLength.value )
 		return rampLength.value
-	return currentTime() - startTime
+	return relativeTime
 }
 
-layout {
-	property( property:rampLength, label:'Ramp Duration\n(s)', min:0 ) 
-	property( property:peakRate, label:'Peak Rate', min:1 ) 
-	property( property:peakLength, label:'Peak Duration\n(s)', min:0 ) 
-	separator( vertical:true )
-	box( widget:'display' ) {
-		node( label:'Rate', content: { if( getT0() > 0 ) String.format( '%7.1f', a*getT0() ) else 0 }, constraints:'wmin 75' )
+def redraw() {
+	def rate = peakRateUnit.value == 'sec' ? a*getT0() : a*getT0()*60
+	layout {
+		property( property:rampLength, label:'Ramp Duration\n(sec)', min:1 )
+		property( property:peakLength, label:'Peak Duration\n(sec)', min:0 )
+		separator( vertical:true )
+		property( property:peakRate, label:'Peak Rate\n(VU/' + peakRateUnit.value + ')', min:1 )
+		property( property:peakRateUnit, label:'Unit', options:['sec','min'] )
+		separator( vertical:true )
+		box( widget:'display' ) {
+			node( label:'Rate', content: { if( getT0() > 0 ) String.format( '%7.1f', rate ) else 0 }, constraints:'w 45!' )
+		}
 	}
 }
+redraw()
 
 def currentTime() {
 	System.currentTimeMillis() / 1000
 }
 
 def calculateAcceleration() {
-	a = peakRate.value / rampLength.value
+	if( peakRateUnit.value == 'sec' )
+		a = peakRate.value / rampLength.value
+	else
+		a = (peakRate.value/60) / rampLength.value
 }
