b80d33553806ff0f8867cb69c95dd22e095051e2
Dain Nilsson <dain.nilsson@smartbear.com>
2012-07-04 12:55:19 +0200

    Bumped version numbers to 2.1.1-SNAPSHOT for REAL this time.

--- loadui-project/loadui-api/src/main/java/com/eviware/loadui/LoadUI.java
+++ loadui-project/loadui-api/src/main/java/com/eviware/loadui/LoadUI.java
@@ -20,7 +20,7 @@ public class LoadUI
 	/**
 	 * The main version number of loadUI.
 	 */
-	public static final String VERSION = "2.1.0";
+	public static final String VERSION = "2.1.0-SNAPSHOT";
 
 	/**
 	 * Internal version number used to determine controller/agent compatibility.
