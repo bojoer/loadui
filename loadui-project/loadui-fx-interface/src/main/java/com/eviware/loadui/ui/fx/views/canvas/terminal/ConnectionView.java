333f905bc5542bcb3e26914f1afc3507984dfe6b
Dain Nilsson <dain.nilsson@smartbear.com>
2012-09-25 10:11:56 +0200

    Made wires Selectable.

--- loadui-project/loadui-fx-interface/src/main/java/com/eviware/loadui/ui/fx/views/canvas/terminal/ConnectionView.java
+++ loadui-project/loadui-fx-interface/src/main/java/com/eviware/loadui/ui/fx/views/canvas/terminal/ConnectionView.java
@@ -1,9 +1,10 @@
 package com.eviware.loadui.ui.fx.views.canvas.terminal;
 
 import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
 import javafx.event.EventHandler;
 import javafx.geometry.Bounds;
-import javafx.scene.layout.Region;
+import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.CubicCurve;
 import javafx.scene.shape.CubicCurveBuilder;
@@ -12,17 +13,20 @@ import com.eviware.loadui.api.terminal.Connection;
 import com.eviware.loadui.api.terminal.InputTerminal;
 import com.eviware.loadui.api.terminal.OutputTerminal;
 import com.eviware.loadui.ui.fx.api.input.DraggableEvent;
+import com.eviware.loadui.ui.fx.api.input.Selectable;
 import com.eviware.loadui.ui.fx.views.canvas.CanvasObjectView;
 import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
 
-public class ConnectionView extends Region
+public class ConnectionView extends Group
 {
 	private final Connection connection;
 	private final OutputTerminalView outputTerminalView;
 	private final InputTerminalView inputTerminalView;
-	private final CubicCurve wire = CubicCurveBuilder.create().fill( Color.TRANSPARENT ).stroke( Color.LIGHTGRAY )
-			.strokeWidth( 8 ).build();
+
+	private final CubicCurve outline = CubicCurveBuilder.create().fill( null ).stroke( Color.BLACK ).strokeWidth( 8 )
+			.build();
+	private final CubicCurve wire = CubicCurveBuilder.create().fill( null ).strokeWidth( 6 ).build();
 
 	public ConnectionView( Connection connection, CanvasObjectView outputComponentView,
 			CanvasObjectView inputComponentView )
@@ -64,7 +68,11 @@ public class ConnectionView extends Region
 		outputComponentView.addEventHandler( DraggableEvent.DRAGGABLE_DRAGGED, eventHandler );
 		inputComponentView.addEventHandler( DraggableEvent.DRAGGABLE_DRAGGED, eventHandler );
 
-		getChildren().add( wire );
+		wire.strokeProperty().bind(
+				Bindings.when( Selectable.installSelectable( this ).selectedProperty() ).then( Color.BLUE )
+						.otherwise( Color.LIGHTGRAY ) );
+
+		getChildren().addAll( outline, wire );
 
 		Platform.runLater( new Runnable()
 		{
@@ -86,6 +94,15 @@ public class ConnectionView extends Region
 		double endX = ( endBounds.getMaxX() + endBounds.getMinX() ) / 2;
 		double endY = ( endBounds.getMaxY() + endBounds.getMinY() ) / 2;
 		double control = Math.min( Math.sqrt( Math.pow( startX - endX, 2 ) + Math.pow( startY - endY, 2 ) ), 200 );
+
+		outline.setStartX( startX );
+		outline.setStartY( startY );
+		outline.setControlX1( startX );
+		outline.setControlY1( startY + control );
+		outline.setControlX2( endX );
+		outline.setControlY2( endY - control );
+		outline.setEndX( endX );
+		outline.setEndY( endY );
 
 		wire.setStartX( startX );
 		wire.setStartY( startY );
