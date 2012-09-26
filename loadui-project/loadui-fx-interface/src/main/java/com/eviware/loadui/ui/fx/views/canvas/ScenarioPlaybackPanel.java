e9a4d0c6d06c6f2e576adb3ba9e527f4f7fd60c1
Dain Nilsson <dain.nilsson@smartbear.com>
2012-09-26 14:23:08 +0200

    Fixed the cell renderer used for the ComboBox used for distribution mode.

--- loadui-project/loadui-fx-interface/src/main/java/com/eviware/loadui/ui/fx/views/canvas/ScenarioPlaybackPanel.java
+++ loadui-project/loadui-fx-interface/src/main/java/com/eviware/loadui/ui/fx/views/canvas/ScenarioPlaybackPanel.java
@@ -8,12 +8,15 @@ import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBuilder;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.StackPaneBuilder;
+import javafx.util.Callback;
 
 import com.eviware.loadui.api.counter.CounterHolder;
 
@@ -58,8 +61,38 @@ public class ScenarioPlaybackPanel extends MiniScenarioPlaybackPanel
 				Bindings.when( playButton.selectedProperty() ).then( "\u25FC" ).otherwise( "\u25B6" ) );
 		StackPane playStack = StackPaneBuilder.create().children( playSpinner, playButton ).build();
 
-		ComboBox<ImageView> distibutionMode = new ComboBox<>( FXCollections.observableArrayList(
-				image( "mode-local.png" ), image( "mode-distributed.png" ) ) );
+		ComboBox<Image> distibutionMode = new ComboBox<>( FXCollections.observableArrayList( image( "mode-local.png" ),
+				image( "mode-distributed.png" ) ) );
+		final Callback<ListView<Image>, ListCell<Image>> cellFactory = new Callback<ListView<Image>, ListCell<Image>>()
+		{
+			@Override
+			public ListCell<Image> call( ListView<Image> arg0 )
+			{
+				return new ListCell<Image>()
+				{
+					private final ImageView imageView = new ImageView();
+
+					@Override
+					protected void updateItem( Image item, boolean empty )
+					{
+						super.updateItem( item, empty );
+
+						if( item == null || empty )
+						{
+							setGraphic( null );
+						}
+						else
+						{
+							imageView.setImage( item );
+							setGraphic( imageView );
+						}
+					}
+				};
+			}
+		};
+		distibutionMode.setCellFactory( cellFactory );
+		distibutionMode.setButtonCell( cellFactory.call( null ) );
+
 		distibutionMode.getSelectionModel().selectFirst();
 
 		time = new CounterDisplay( "Time" );
@@ -74,8 +107,8 @@ public class ScenarioPlaybackPanel extends MiniScenarioPlaybackPanel
 				failures, separator(), resetButton, limitsButton );
 	}
 
-	private ImageView image( String name )
+	private Image image( String name )
 	{
-		return new ImageView( new Image( getClass().getResourceAsStream( name ) ) );
+		return new Image( getClass().getResourceAsStream( name ) );
 	}
 }
