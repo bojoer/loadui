50401e3fa7fedadae66d825d7ce60307969ade7e
Dain Nilsson <dain.nilsson@smartbear.com>
2012-10-19 15:51:27 +0200

    Added OnOff switch to OnOffComponent.

--- loadui-project/loadui-fx-interface/src/main/java/com/eviware/loadui/ui/fx/views/canvas/component/OnOffComponentView.java
+++ loadui-project/loadui-fx-interface/src/main/java/com/eviware/loadui/ui/fx/views/canvas/component/OnOffComponentView.java
@@ -1,16 +1,21 @@
 package com.eviware.loadui.ui.fx.views.canvas.component;
 
-import javafx.scene.control.ToggleButton;
-import javafx.scene.control.ToggleButtonBuilder;
+import java.util.Arrays;
+
+import javafx.beans.property.Property;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.layout.HBoxBuilder;
 
 import com.eviware.loadui.api.component.categories.OnOffCategory;
 import com.eviware.loadui.api.model.ComponentItem;
+import com.eviware.loadui.ui.fx.control.OptionsSlider;
 import com.eviware.loadui.ui.fx.util.Properties;
 
 public class OnOffComponentView extends ComponentView
 {
-	private final ToggleButton onOffButton = ToggleButtonBuilder.create().text( "ON" ).selected( true ).build();
+	private final OptionsSlider onOffSlider = new OptionsSlider( Arrays.asList( "ON", "OFF" ) );
+	private final Property<Boolean> stateProperty;
 
 	protected OnOffComponentView( ComponentItem component )
 	{
@@ -18,9 +23,30 @@ public class OnOffComponentView extends ComponentView
 
 		getStyleClass().add( "on-off" );
 
-		topBar.setLeft( HBoxBuilder.create().spacing( 3 ).children( onOffButton, topBar.getLeft() ).build() );
+		onOffSlider.getStyleClass().add( "switch" );
 
-		onOffButton.selectedProperty().bindBidirectional(
-				Properties.convert( ( ( OnOffCategory )component.getBehavior() ).getStateProperty() ) );
+		topBar.setLeft( HBoxBuilder.create().spacing( 3 ).children( onOffSlider, topBar.getLeft() ).build() );
+
+		stateProperty = Properties.convert( ( ( OnOffCategory )component.getBehavior() ).getStateProperty() );
+
+		stateProperty.addListener( new ChangeListener<Boolean>()
+		{
+			@Override
+			public void changed( ObservableValue<? extends Boolean> arg0, Boolean arg1, Boolean state )
+			{
+				onOffSlider.setSelected( state.booleanValue() ? "ON" : "OFF" );
+			}
+		} );
+
+		onOffSlider.setSelected( stateProperty.getValue().booleanValue() ? "ON" : "OFF" );
+
+		onOffSlider.selectedProperty().addListener( new ChangeListener<String>()
+		{
+			@Override
+			public void changed( ObservableValue<? extends String> arg0, String arg1, String state )
+			{
+				stateProperty.setValue( "ON".equals( state ) );
+			}
+		} );
 	}
 }
