db6c5a918adbc39c94a08a42ef9b4344fa5ad47d
Dain Nilsson <dain.nilsson@smartbear.com>
2012-10-23 13:50:17 +0200

    Improved EventLogInspector.

--- loadui-project/loadui-fx-interface/src/main/java/com/eviware/loadui/ui/fx/views/eventlog/EventLogView.java
+++ loadui-project/loadui-fx-interface/src/main/java/com/eviware/loadui/ui/fx/views/eventlog/EventLogView.java
@@ -4,6 +4,7 @@ import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.TableColumnBuilder;
 import javafx.scene.control.TableView;
 import javafx.util.Callback;
 
@@ -16,9 +17,9 @@ public class EventLogView extends TableView<TestEvent.Entry>
 	@SuppressWarnings( "unchecked" )
 	public EventLogView()
 	{
-		TableColumn<Entry, String> timeColumn = new TableColumn<>( "Time" );
-		timeColumn
-				.setCellValueFactory( new Callback<TableColumn.CellDataFeatures<Entry, String>, ObservableValue<String>>()
+		TableColumn<Entry, String> timeColumn = TableColumnBuilder.<Entry, String> create().text( "Time" )
+				.sortable( false ).minWidth( 80 )
+				.cellValueFactory( new Callback<TableColumn.CellDataFeatures<Entry, String>, ObservableValue<String>>()
 				{
 					@Override
 					public ObservableValue<String> call( CellDataFeatures<Entry, String> features )
@@ -26,40 +27,40 @@ public class EventLogView extends TableView<TestEvent.Entry>
 						return new ReadOnlyStringWrapper( FormattingUtils.formatTimeMillis( features.getValue()
 								.getTestEvent().getTimestamp() ) );
 					}
-				} );
+				} ).build();
 
-		TableColumn<Entry, String> eventTypeColumn = new TableColumn<>( "Event Type" );
-		eventTypeColumn
-				.setCellValueFactory( new Callback<TableColumn.CellDataFeatures<Entry, String>, ObservableValue<String>>()
+		TableColumn<Entry, String> eventTypeColumn = TableColumnBuilder.<Entry, String> create().text( "Event Type" )
+				.sortable( false ).minWidth( 100 )
+				.cellValueFactory( new Callback<TableColumn.CellDataFeatures<Entry, String>, ObservableValue<String>>()
 				{
 					@Override
 					public ObservableValue<String> call( CellDataFeatures<Entry, String> features )
 					{
 						return new ReadOnlyStringWrapper( features.getValue().getTypeLabel() );
 					}
-				} );
+				} ).build();
 
-		TableColumn<Entry, String> eventSourceColumn = new TableColumn<>( "Event Source" );
-		eventTypeColumn
-				.setCellValueFactory( new Callback<TableColumn.CellDataFeatures<Entry, String>, ObservableValue<String>>()
+		TableColumn<Entry, String> eventSourceColumn = TableColumnBuilder.<Entry, String> create().text( "Event Source" )
+				.sortable( false ).minWidth( 100 )
+				.cellValueFactory( new Callback<TableColumn.CellDataFeatures<Entry, String>, ObservableValue<String>>()
 				{
 					@Override
 					public ObservableValue<String> call( CellDataFeatures<Entry, String> features )
 					{
 						return new ReadOnlyStringWrapper( features.getValue().getSourceLabel() );
 					}
-				} );
+				} ).build();
 
-		TableColumn<Entry, String> descriptionColumn = new TableColumn<>( "Description" );
-		eventTypeColumn
-				.setCellValueFactory( new Callback<TableColumn.CellDataFeatures<Entry, String>, ObservableValue<String>>()
+		TableColumn<Entry, String> descriptionColumn = TableColumnBuilder.<Entry, String> create().text( "Description" )
+				.sortable( false ).prefWidth( 300 )
+				.cellValueFactory( new Callback<TableColumn.CellDataFeatures<Entry, String>, ObservableValue<String>>()
 				{
 					@Override
 					public ObservableValue<String> call( CellDataFeatures<Entry, String> features )
 					{
 						return new ReadOnlyStringWrapper( features.getValue().getTestEvent().toString() );
 					}
-				} );
+				} ).build();
 
 		getColumns().setAll( timeColumn, eventTypeColumn, eventSourceColumn, descriptionColumn );
 	}
