10f63c091d7dd0bb9bb5a485cb621f8358c5568c
Dain Nilsson <dain.nilsson@smartbear.com>
2012-11-01 17:20:12 +0100

    Added JavaFX Property getters for text on PageList and ToolBox

--- loadui-project/loadui-fx-interface/src/main/java/com/eviware/loadui/ui/fx/views/result/ResultView.java
+++ loadui-project/loadui-fx-interface/src/main/java/com/eviware/loadui/ui/fx/views/result/ResultView.java
@@ -3,10 +3,10 @@ package com.eviware.loadui.ui.fx.views.result;
 import javafx.beans.binding.Bindings;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
 import javafx.scene.layout.StackPane;
 
 import com.eviware.loadui.api.statistics.store.Execution;
+import com.eviware.loadui.ui.fx.control.PageList;
 import com.eviware.loadui.ui.fx.util.FXMLUtils;
 import com.eviware.loadui.ui.fx.util.ObservableLists;
 import com.google.common.base.Function;
@@ -14,7 +14,7 @@ import com.google.common.base.Function;
 public class ResultView extends StackPane
 {
 	@FXML
-	private ListView<ExecutionNode> resultNodeList;
+	private PageList<ExecutionNode> resultNodeList;
 
 	private final ObservableList<Execution> executionList;
 
