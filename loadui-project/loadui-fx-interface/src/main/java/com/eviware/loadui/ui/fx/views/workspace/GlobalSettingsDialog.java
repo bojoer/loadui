42d3e56e9622e4e89910ed7c1ab2e3fa951e1591
Renato Athaydes <renato.deathaydes@smartbear.com>
2012-12-10 13:47:10 +0100

    Modified names of tabs and dialog title for the Global Settings dialog.

--- loadui-project/loadui-fx-interface/src/main/java/com/eviware/loadui/ui/fx/views/workspace/GlobalSettingsDialog.java
+++ loadui-project/loadui-fx-interface/src/main/java/com/eviware/loadui/ui/fx/views/workspace/GlobalSettingsDialog.java
@@ -10,21 +10,21 @@ import com.eviware.loadui.ui.fx.control.SettingsTab;
 import com.eviware.loadui.ui.fx.control.SettingsTab.Builder;
 import com.google.common.collect.Lists;
 
-@Deprecated
 public class GlobalSettingsDialog
 {
 	public static SettingsDialog newInstance( @Nonnull Node parent, @Nonnull WorkspaceItem workspace )
 	{
-		SettingsTab generalTab = Builder.create( "Global Settings" )
+		SettingsTab generalTab = Builder.create( "Execution" )
 				.field( "Max internal threads", workspace.getProperty( WorkspaceItem.MAX_THREADS_PROPERTY ) )
 				.field( "Max internal thread queue size", workspace.getProperty( WorkspaceItem.MAX_THREAD_QUEUE_PROPERTY ) )
 				.build();
-		
-		SettingsTab statsTab = Builder.create( "Statistics Settings" )
+
+		SettingsTab statsTab = Builder
+				.create( "Statistics" )
 				.field( "Results path (affects all projects)", workspace.getProperty( WorkspaceItem.STATISTIC_RESULTS_PATH ) )
-				.field( "Number of results to autosave", workspace.getProperty( WorkspaceItem.STATISTIC_NUMBER_OF_AUTOSAVES ) )
-				.build();
-		
-		return new SettingsDialog( parent, "Execution", Lists.newArrayList( generalTab, statsTab ) );
+				.field( "Number of results to autosave",
+						workspace.getProperty( WorkspaceItem.STATISTIC_NUMBER_OF_AUTOSAVES ) ).build();
+
+		return new SettingsDialog( parent, "Global settings", Lists.newArrayList( generalTab, statsTab ) );
 	}
 }
