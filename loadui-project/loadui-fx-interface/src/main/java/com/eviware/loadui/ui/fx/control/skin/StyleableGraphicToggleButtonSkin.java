2ee1874c89b579d0f94bae45a9a6425eb252d89a
Mikael Östberg <mikael.ostberg@smartbear.com>
2013-02-14 15:51:24 +0100

    Made a button in the linechart segment-box easier to click and more according to the design

---
+++ loadui-project/loadui-fx-interface/src/main/java/com/eviware/loadui/ui/fx/control/skin/StyleableGraphicToggleButtonSkin.java
@@ -0,0 +1,19 @@
+package com.eviware.loadui.ui.fx.control.skin;
+
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.HBoxBuilder;
+import javafx.scene.layout.RegionBuilder;
+
+import com.sun.javafx.scene.control.skin.ToggleButtonSkin;
+
+public class StyleableGraphicToggleButtonSkin extends ToggleButtonSkin
+{
+
+	public StyleableGraphicToggleButtonSkin( ToggleButton button )
+	{
+		super( button );
+		
+		button.setGraphic( HBoxBuilder.create().children(RegionBuilder.create().styleClass( "graphic" ).build(), RegionBuilder.create().styleClass( "graphic" ).build()).build());
+	}
+
+}
