05cadc87f49d568d1a8ff09d6a91a9dbc51a11bf
Mikael Östberg <mikael.ostberg@smartbear.com>
2013-02-21 16:10:38 +0100

    Styled terminalconnectors, including the draggable node.

--- loadui-project/loadui-fx-interface/src/main/java/com/eviware/loadui/ui/fx/views/canvas/terminal/TerminalView.java
+++ loadui-project/loadui-fx-interface/src/main/java/com/eviware/loadui/ui/fx/views/canvas/terminal/TerminalView.java
@@ -6,7 +6,6 @@ import javafx.scene.Node;
 import javafx.scene.control.Tooltip;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.CircleBuilder;
 
 import com.eviware.loadui.api.terminal.Terminal;
@@ -14,63 +13,58 @@ import com.eviware.loadui.ui.fx.control.DragNode;
 import com.eviware.loadui.ui.fx.util.FXMLUtils;
 import com.eviware.loadui.ui.fx.util.Properties;
 
-public class TerminalView extends StackPane
-{
+public class TerminalView extends StackPane {
 	private final Terminal terminal;
 	private Runnable onLayout;
 
-	public TerminalView( Terminal terminal )
-	{
+	public TerminalView(Terminal terminal) {
 		this.terminal = terminal;
-		FXMLUtils.load( this, this, TerminalView.class.getResource( TerminalView.class.getSimpleName() + ".fxml" ) );
+		FXMLUtils.load(
+				this,
+				this,
+				TerminalView.class.getResource(TerminalView.class
+						.getSimpleName() + ".fxml"));
 	}
 
 	@FXML
 	Node terminalNode;
 
 	@FXML
-	protected void initialize()
-	{
-		final DragNode dragNode = DragNode.install( terminalNode, CircleBuilder.create().radius( 10 ).fill( Color.GREEN )
-				.build() );
-		dragNode.setRevert( false );
-		dragNode.setData( terminal );
+	protected void initialize() {
+		final DragNode dragNode = DragNode.install(terminalNode, CircleBuilder
+				.create().radius(10).style("-fx-fill: radial-gradient(center 50% 50%, radius 50%, reflect, #BBBBBB, #888888, #444444);").build());
+		dragNode.setRevert(false);
+		dragNode.setData(terminal);
 
 		Tooltip tooltip = new Tooltip();
-		tooltip.textProperty().bind( Properties.forDescription( terminal ) );
-		Tooltip.install( terminalNode, tooltip );
+		tooltip.textProperty().bind(Properties.forDescription(terminal));
+		Tooltip.install(terminalNode, tooltip);
 
-		terminalNode.addEventHandler( MouseEvent.ANY, new EventHandler<MouseEvent>()
-		{
-			@Override
-			public void handle( MouseEvent event )
-			{
-				event.consume();
-			}
-		} );
+		terminalNode.addEventHandler(MouseEvent.ANY,
+				new EventHandler<MouseEvent>() {
+					@Override
+					public void handle(MouseEvent event) {
+						event.consume();
+					}
+				});
 	}
 
-	public Terminal getTerminal()
-	{
+	public Terminal getTerminal() {
 		return terminal;
 	}
 
-	public Runnable getOnLayout()
-	{
+	public Runnable getOnLayout() {
 		return onLayout;
 	}
 
-	public void setOnLayout( Runnable onLayout )
-	{
+	public void setOnLayout(Runnable onLayout) {
 		this.onLayout = onLayout;
 	}
 
 	@Override
-	protected void layoutChildren()
-	{
+	protected void layoutChildren() {
 		super.layoutChildren();
-		if( onLayout != null )
-		{
+		if (onLayout != null) {
 			onLayout.run();
 		}
 	}
